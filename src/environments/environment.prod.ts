a61439b2f65617278765c35d22ff6a07986fdec7
AMIT VERMA <amit.verma@softobotics.com>
2024-10-04 14:21:57 +0530

    code added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     API_URL: 'https://api-18dg.onrender.com/api',
+    IMAGE_URL: 'https://api-18dg.onrender.com',
     UI_URL: 'https://kapil-coin.onrender.com',
 }
