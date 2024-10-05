71371a1d41cab4826f6167542a4b507af346ba33
AMIT VERMA <amit.verma@softobotics.com>
2024-10-05 10:45:27 +0530

    code update

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -3,7 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
-import { CookieService } from '../../services/cookie.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-users-table',
@@ -13,22 +13,43 @@ import { CookieService } from '../../services/cookie.service';
 export class UsersComponent implements OnInit {
   displayedColumns: string[] = ['userId', 'name', 'email', 'mobile', 'emailVerified', 'referralCode', 'createdAt', 'status', 'action'];
   dataSource = new MatTableDataSource<any>();
-  
+  searchQuery: FormControl<string | null> = new FormControl(null); // Specify the type here
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private usersService: UsersService, private router: Router, public cookie:CookieService) {}
+  constructor(private usersService: UsersService, private router: Router) {}
 
   ngOnInit(): void {
+    this.fetchUsers();
 
+    // Subscribe to value changes in the search field
+    this.searchQuery.valueChanges.subscribe(() => {
+      this.applyFilter();
+    });
+  }
+
+  fetchUsers(): void {
     this.usersService.getUsers().subscribe(
-      (response:any) => {
+      (response: any) => {
         this.dataSource.data = response;
         this.dataSource.paginator = this.paginator; // Set the paginator
       },
-      (error:any) => {
+      (error: any) => {
         console.error('Error fetching users', error);
       }
     );
+  }
+
+  applyFilter(): void {
+    const filterValue = this.searchQuery.value?.trim().toLowerCase() || ''; // Ensure it's always a string
+
+    // Set a custom filter predicate
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      const searchText = filter.toLowerCase();
+      return data.name.toLowerCase().includes(searchText) || data.email.toLowerCase().includes(searchText);
+    };
+
+    this.dataSource.filter = filterValue; // Trigger the filtering
   }
 
   getStatusClass(status: string): string {
@@ -44,13 +65,14 @@ export class UsersComponent implements OnInit {
     }
   }
 
-  goToUserNetwork(userId: number) {
+  goToUserNetwork(userId: number): void {
     this.router.navigate(['/friends-network', userId]);
   }
 
   goToUserDetails(userId: number): void {
     this.router.navigate(['/user-details', userId]);
   }
+
   goToPaymentDetails(userId: number): void {
     this.router.navigate(['/payment-details', userId]);
   }
