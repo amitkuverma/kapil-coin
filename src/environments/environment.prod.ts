042f75c324410c7dfe7f5f7f6eb6185e7ce541da
AMIT VERMA <amit.verma@softobotics.com>
2024-10-11 14:13:25 +0530

    code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'https://api-gorkhacoin.com/api',
-    IMAGE_URL: 'https://api-gorkhacoin.com/',
+    API_URL: 'https://api.gorkhacoin.com/api',
+    IMAGE_URL: 'https://api.gorkhacoin.com/',
     UI_URL: 'https://gorkhacoin.com',
 }
