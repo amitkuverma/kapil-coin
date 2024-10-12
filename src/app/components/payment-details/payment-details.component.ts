ecf2476a5a8dffb2de440e644b5625bb00b03fee
AMIT VERMA <amit.verma@softobotics.com>
2024-10-12 09:53:34 +0530

    cd

--- src/app/components/payment-details/payment-details.component.ts
+++ src/app/components/payment-details/payment-details.component.ts
@@ -1,10 +1,11 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaymentService } from '../../services/payment.service';
 import { UsersService } from '../../services/users.service';
 import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-payment-details',
@@ -16,8 +17,9 @@ export class PaymentComponent {
   isLoading: boolean = false;
   userId:any;
   imageUrl:any;
+  @ViewChild('shareDialog') shareDialog!: TemplateRef<any>;
 
-  constructor(private usersService: UsersService, private route: ActivatedRoute, public location: Location, private paymentService: PaymentService,
+  constructor(public dialog: MatDialog, private usersService: UsersService, private route: ActivatedRoute, public location: Location, private paymentService: PaymentService,
     private toastr: ToastrService
   ) { 
     this.imageUrl = environment.IMAGE_URL
@@ -25,10 +27,15 @@ export class PaymentComponent {
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('userId');
+    this.getPaymentData();
+  }
+
+  getPaymentData(){
     this.paymentService.getUserReferrals(this.userId).subscribe((data: any) => {
       this.paymentDetails = data;
     });
   }
+
   updateStatus(userId: number, status: string): void {
     this.isLoading = true
     this.usersService.updateUserStatus(userId, status).subscribe(
@@ -43,4 +50,21 @@ export class PaymentComponent {
     );
   }
 
+  openShareDialog() {
+    this.dialog.open(this.shareDialog);
+  }
+
+  addPayment(): void {
+    this.isLoading = true    
+    this.paymentService.updateUserStatus(this.paymentDetails, this.paymentDetails.payId).subscribe(
+      (res: any) => {
+        this.isLoading = false;
+        this.location.back();
+      },
+      (error: any) => {
+        this.isLoading = false;
+        this.toastr.error(error, "Error")
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,6 @@
 export const environment = {
     production: false,
-    // API_URL: 'http://localhost:3000/api',
-    // IMAGE_URL: 'http://localhost:3000/',
-    // UI_URL: 'http://localhost:4200',
-    API_URL: 'https://gorkhacoin-api.gyanvidigital.xyz/api',
-    IMAGE_URL: 'https://gorkhacoin-api.gyanvidigital.xyz',
-    UI_URL: 'https://gorkhacoins.gyanvidigital.xyz',
+    API_URL: 'http://localhost:8081/api',
+    IMAGE_URL: 'http://localhost:8081/',
+    UI_URL: 'http://localhost:4200',
 }
