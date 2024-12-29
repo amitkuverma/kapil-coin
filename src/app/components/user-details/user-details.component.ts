764eef38b0d334c31c7b0de8d0157eab904d1daa
AMIT VERMA <amit.verma@softobotics.com>
2024-12-30 00:20:35 +0530

    dfd

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -4,6 +4,7 @@ import { UsersService } from '../../services/users.service';
 import { Location } from '@angular/common';
 import { CookieService } from '../../services/cookie.service';
 import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-details',
@@ -12,11 +13,14 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class UserDetailsComponent implements OnInit {
   user: any;
+  imageUrl:any;
   isLoading: boolean = false;
 
   constructor(private usersService: UsersService, private route: ActivatedRoute, public location: Location, public cookies: CookieService,
     private toastr: ToastrService
-  ) { }
+  ) {
+    this.imageUrl = environment.IMAGE_URL
+   }
 
   ngOnInit(): void {
     const userId = this.route.snapshot.paramMap.get('userId');
